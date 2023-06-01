d16f9f5125d7bf29b89801392118e8e21af5a1e7
Smoup <106016152+Smoup@users.noreply.github.com>
2023-06-01 21:47:34 +0300

    init

---
+++ src/main/java/org/smod/SMod_ChatFilter_Fabric.java
@@ -0,0 +1,10 @@
+package org.smod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SMod_ChatFilter_Fabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/smod/client/SMod_ChatFilter_FabricClient.java
@@ -0,0 +1,10 @@
+package org.smod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SMod_ChatFilter_FabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
